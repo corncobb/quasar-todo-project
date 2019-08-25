ccbf6617f449539a8512fbe14425441e99ed74f1
Cameron Cobb <cameronjcobb@gmail.com>
2019-08-25 15:43:10 -0700

    added some stuff

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="q-pa-lg">
+    <input v-model="message"
+    @keyup.esc="clearMessage"
+    @keyup.enter="alertMessage"/>
+    <button @click="clearMessage">clear</button>
+    <h5 class="border-black" v-show="message.length">{{ message }} </h5>
   </q-page>
 </template>
 
-<style>
-</style>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data(){
+    return{
+      message: "I love vue.js"
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = ''
+    },
+    alertMessage(){
+      alert(this.message)
+    }
+  }
 }
 </script>
+
+<style>
+.border-black{
+  border: 1px solid black;
+}
+</style>
