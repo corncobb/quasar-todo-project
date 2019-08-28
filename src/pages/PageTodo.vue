d21e08d3ef1a226aea778859ec97ac364aa56523
Cameron Cobb <cameronjcobb@gmail.com>
2019-08-27 17:02:13 -0700

    added deleting

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,35 +1,11 @@
 <template>
   <q-page class="q-pa-lg">
     <q-list separator bordered>
-
-      <q-item 
+      <task 
       v-for="(task, key) in tasks"
       :key="key"
-      @click="task.completed = !task.completed"
-      :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-      clickable
-      v-ripple>
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label
-          :class="{ 'text-strikethrough' : task.completed }">{{ task.name }}</q-item-label>
-        </q-item-section>
-
-        <q-item-section side>
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon name="event" size="18px" class="q-mr-md"/>
-            </div>
-            <div class="column">
-              <q-item-label class="row justify-end" caption>{{ task.dueDate }}</q-item-label>
-              <q-item-label class="row justify-end" caption><small>{{ task.dueTime }}</small></q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-      </q-item>
+      :task="task"
+      :id="key"></task>
     </q-list>
   </q-page>
 </template>
@@ -42,6 +18,9 @@ import { mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters('tasks', ['tasks'])
+  },
+  components: {
+    'task': require('components/Tasks/Task.vue').default
   }
 }
 
