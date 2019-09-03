f3df3572bc01cc8eb30726f9b3afe8150e2a4aa3
Cameron Cobb <cameronjcobb@gmail.com>
2019-09-03 16:40:47 -0700

    adding sorting

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -22,7 +22,8 @@ const state = {
             dueTime: "1:30"
         }
     },
-    search: ''
+    search: '',
+    sort: 'name'
 }
 
 const mutations = {
@@ -64,11 +65,31 @@ const actions = {
 }
 
 const getters = {
-    tasksFiltered: (state) => {
-        let tasksFiltered = {}
+    tasksSorted: (state) => {
+        let tasksSorted = {},
+            keysOrdered = Object.keys(state.tasks)
+
+        keysOrdered.sort((a,b) => {
+            let taskAProp = state.tasks[a][state.sort].toLowerCase(),
+                taskBProp = state.tasks[b][state.sort].toLowerCase()
+
+            if (taskAProp > taskBProp) return 1
+            else if (taskAProp < taskBProp) return -1
+            else return 0
+        })
+        keysOrdered.forEach((key) => {
+            tasksSorted[key] = state.tasks[key]
+        })
+
+        return tasksSorted
+    },
+    tasksFiltered: (state, getters) => {
+        let tasksSorted = getters.tasksSorted,
+            tasksFiltered = {}
+     
         if (state.search) {
-            Object.keys(state.tasks).forEach(function(key) {
-                let task = state.tasks[key],
+            Object.keys(tasksSorted).forEach(function(key) {
+                let task = tasksSorted[key],
                     taskNameLowerCase = task.name.toLowerCase(),
                     searchLowerCase = state.search.toLowerCase()
                 if (taskNameLowerCase.includes(searchLowerCase)){
@@ -78,7 +99,7 @@ const getters = {
             // populate empty object
             return tasksFiltered
         }
-        return state.tasks
+        return tasksSorted
     },
     tasksTodo: (state, getters) => {
         let tasksFiltered = getters.tasksFiltered
