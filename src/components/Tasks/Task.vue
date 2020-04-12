c25c70afaf8c6e8f5865fd09b8a1b5581460fce8
Cameron Cobb <cameronjcobb@gmail.com>
2020-04-12 01:05:20 -0700

    fixed annoying bug that wouldn't display if a task was completed or not

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -1,92 +1,86 @@
 <template>
-    <q-item
-      @click="updateTask({ id: id, updates: { completed: !task.completed} })"
-      :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-      clickable
-      v-touch-hold:1000.mouse="showEditTaskModal"
-      v-ripple>
-        <q-item-section side top>
-          <q-checkbox :value="task.completed" class="no-pointer-events" />
-        </q-item-section>
+  <q-item 
+  	@click="updateTask({ id: id, updates: { completed: !task.completed } })"
+  	:class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
+    v-touch-hold:1000.mouse="showEditTaskModal"
+  	clickable
+  	v-ripple>
+    <q-item-section side top>
+      <q-checkbox
+        v-model="task.completed"
+        class="no-pointer-events" />
+    </q-item-section>
 
-        <q-item-section>
+    <q-item-section>
+      <q-item-label
+      	:class="{ 'text-strikethrough' : task.completed }"
+        v-html="$options.filters.searchHighlight(task.name, search)">
+      </q-item-label>
+    </q-item-section>
+
+    <q-item-section 
+      v-if="task.dueDate"
+      side>
+    	<div class="row">
+    		<div class="column justify-center">
+		    	<q-icon 
+		    		name="event"
+		    		size="18px"
+		    		class="q-mr-xs" />
+    		</div>
+	    	<div class="column">
+          <q-item-label 
+          	class="row justify-end"
+          	caption>
+          	{{ task.dueDate | niceDate }}
+          </q-item-label>
           <q-item-label
-          :class="{ 'text-strikethrough' : task.completed }"
-          v-html="$options.filters.searchHighlight(task.name, search)"></q-item-label>
-        </q-item-section>
+          	class="row justify-end"
+          	caption>
+          	<small>{{ taskDueTime }}</small>
+          </q-item-label>
+	    	</div>
+    	</div>
+    </q-item-section>
 
-        <q-item-section 
-        side
-        v-if="task.dueDate">
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon name="event" size="18px" class="q-mr-md"/>
-            </div>
-            <div class="column">
-              <q-item-label class="row justify-end" caption>{{ task.dueDate | niceDate }}</q-item-label>
-              <q-item-label class="row justify-end" caption><small>{{ taskDueTime }}</small></q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-        <q-item-section side>
-          <div class="row">
-            <q-btn
-              @click.stop="showEditTaskModal"
-              flat
-              round 
-              dense 
-              color="primary" 
-              icon="edit" />
-            <q-btn
-              @click.stop="showDeletePopup = true"
-              flat
-              round 
-              dense 
-              color="red" 
-              icon="delete" />
-          </div>
-        </q-item-section>
+    <q-item-section side>
+      <div class="row">
+        <q-btn
+          @click.stop="showEditTaskModal"
+          flat
+          round
+          dense
+          color="primary"
+          icon="edit" />
+        <q-btn
+          @click.stop="promptToDelete(id)"
+          flat
+          round
+          dense
+          color="red"
+          icon="delete" />
+      </div>
+    </q-item-section>
 
-        <q-dialog v-model="showEditTask">
-            <edit-task
-              @close="showEditTask = false"
-              :task="task"
-              :id="id"/>
-        </q-dialog>
-
-        <q-dialog persistent v-model="showDeletePopup">
-            <q-card style="width: 300px">
-              <q-card-section class="row">
-                 <div class="text-h4">Confirm</div>
-              </q-card-section>
-
-              <q-card-section class="row">
-                 <div class="text-body1">Do you really want to delete this task?</div>
-              </q-card-section>
-
-              <q-card-actions align="right">
-                <q-btn label="Cancel" color="red" v-close-popup />
-                <q-btn label="OK" @click="deleteThisTask(id)" color="primary" v-close-popup />
-              </q-card-actions>
-            </q-card>
-        </q-dialog>
-
-      </q-item>
+    <q-dialog v-model="showEditTask">
+      <edit-task 
+        @close="showEditTask = false" 
+        :task="task"
+        :id="id" />
+    </q-dialog>
     
+  </q-item>
 </template>
 
 <script>
-import { mapState, mapActions, mapGetters } from 'vuex'
-import { date } from 'quasar'
-// destructuring to keep only what is needed
-const { addToDate } = date
+  import { mapState, mapActions, mapGetters } from 'vuex'
+  import { date } from 'quasar'
 
-export default {
-    props: ['task', 'id'],
+	export default {
+		props: ['task', 'id'],
     data() {
-      return{
-        showEditTask: false,
-        showDeletePopup: false
+      return {
+        showEditTask: false
       }
     },
     computed: {
@@ -94,11 +88,31 @@ export default {
       ...mapGetters('settings', ['settings']),
       taskDueTime() {
         if (this.settings.show12HourTimeFormat) {
-
-          return date.formatDate(this.task.dueDate + ' ' + this.task.dueTime, 'h:mm A')
+          return date.formatDate(this.task.dueDate + ' ' + this.task.dueTime, 'h:mmA')
         }
         return this.task.dueTime
       }
+    },
+    methods: {
+      ...mapActions('tasks', ['updateTask', 'deleteTask']),
+      showEditTaskModal() {
+        this.showEditTask = true
+      },
+      promptToDelete(id) {
+        this.$q.dialog({
+          title: 'Confirm',
+          message: 'Really delete?',
+          ok: {
+            push: true
+          },
+          cancel: {
+            color: 'negative'
+          },
+          persistent: true
+        }).onOk(() => {
+          this.deleteTask(id)
+        })
+      } 
     },
     filters: {
       niceDate(value) {
@@ -111,25 +125,15 @@ export default {
             return '<span class="bg-yellow-6">' + match + '</span>'
           })
         }
-       return value
+        return value
       }
-    },
-    methods: {
-        ...mapActions('tasks', ['updateTask', 'deleteTask']),
-        deleteThisTask(id){
-          this.deleteTask(id);
-        },
-        showEditTaskModal() {
-          this.showEditTask = true
-        }
     },
     components: {
       'edit-task': require('components/Tasks/Modals/EditTask.vue').default
     }
-}
-
+	}
 </script>
 
-<style scoped>
-
+<style>
+	
 </style>
--- src/components/Tasks/TasksTodo.vue
+++ src/components/Tasks/TasksTodo.vue
@@ -1,39 +1,41 @@
 <template>
-     <transition
-        appear
-        enter-active-class="animated zoomIn"
-        leave-active-class="animated zoomOut absolute-top"
-    >
-        <div>
-            <list-header
-                v-if="!settings.showTasksInOneList"
-                bgColor="bg-orange-4">Todo</list-header>
-            <q-list 
-                v-if="Object.keys(tasksTodo).length"
-                separator 
-                bordered>
+	<transition
+	  appear
+	  enter-active-class="animated zoomIn"
+	  leave-active-class="animated zoomOut absolute-top"
+	>
+		<div>
+			<list-header
+				v-if="!settings.showTasksInOneList"
+				bgColor="bg-orange-4"
+				>Todo</list-header>
 
-                <task 
-                v-for="(task, key) in tasksTodo"
-                :key="key"
-                :task="task"
-                :id="key"></task>
-            </q-list>
-        </div>
-     </transition>
+			<q-list 
+				separator
+				bordered>
+
+				<task
+					v-for="(task, key) in tasksTodo"
+			  	:key="key"
+			  	:task="task"
+			  	:id="key"></task>
+
+			</q-list>
+		</div>
+	</transition>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+	import { mapGetters } from 'vuex'
 
-export default {
-    props: ['tasksTodo'],
-    computed: {
-        ...mapGetters('settings', ['settings'])
-    },
-    components: {
-        'task': require('components/Tasks/Task.vue').default,
-        'list-header': require('components/Shared/ListHeader.vue').default,
-    }
-}
+	export default {
+		props: ['tasksTodo'],
+		computed: {
+			...mapGetters('settings', ['settings'])
+		},
+		components: {
+			'task' : require('components/Tasks/Task.vue').default,
+			'list-header' : require('components/Shared/ListHeader.vue').default,
+		}
+	}
 </script>
